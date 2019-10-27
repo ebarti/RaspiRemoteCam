1eae4a1426af7b0187d8b00fa491d5ceb69ee4fc
EBA5 <eloibarti@gmail.com>
2019-10-27 20:54:56 +0100

    Updates

--- libs/ControlSystem/include/ControlSystem.h
+++ libs/ControlSystem/include/ControlSystem.h
@@ -11,15 +11,42 @@ extern "C++" {
 class ControlSystem
 {
 public:
-	ControlSystem(int iTiltGPIOidx, int iPanGPIOidx);
+	ControlSystem();
 	~ControlSystem();
+	/*
+	@param iTiltGPIOidx:
+		The GPIO index of the tilt servo within the RPI board. The servo must be connected to a USER GPIO, range index = 0-31
+	@param iPanGPIOidx:
+	The GPIO index of the pan servo within the RPI board. The servo must be connected to a USER GPIO, range index = 0-31
+	@return:
+		0 = Succeeded
+		1 = Error initializing (either bad GPIO or bad PulseWidth)
+	*/
+	int InitializeServos(int iTiltGPIOidx, int iPanGPIOidx);
 
-	int Initialize();
+	/*
+	@param iCascadePath:
+		Open CV cascade classifier file path (.xml) for feature detection (face detection in our case)
+	@param iNestedCascadePath:
+		OpenCV nested cascade clasifier file path (.xml) for a nested feature detection
+	@param iScale:
+		The scale of the images retrieved by the raspberry cam. Typically 1
+	@return:
+		0 = Succeeded
+		1 = Error initializing (either bad GPIO or bad PulseWidth)
+	*/
+	int InitializeCamera(std::string iCascadePath, std::string iNestedCascadePath, double iScale = 1.0);
+
+
+	int ActivateTrackingMode();
+
+	int ActivateUIMode();
 
 private:
-
-	sg90ctl tiltCtl;
 	
+	FaceTrackingCamera* rpiCam;
+	sg90ctl* tiltCtl;
+	sg90ctl* panCtl;
 };
 
 
--- libs/ControlSystem/src/ControlSystem.cpp
+++ libs/ControlSystem/src/ControlSystem.cpp
@@ -1,1 +1,56 @@
+#ifndef __cplusplus // C++ compatibility
+extern "C++" {
+#endif //#ifndef __cplusplus
+
 #include "ControlSystem.h"
+
+ControlSystem::ControlSystem()
+{
+	rpiCam = NULL;
+	tiltCtl = NULL;
+	panCtl = NULL;
+}
+ControlSystem::~ControlSystem()
+{
+	if(rpiCam)
+		delete rpiCam;
+	rpiCam = NULL;
+	if (tiltCtl)
+		delete tiltCtl;
+	tiltCtl = NULL;
+	if (panCtl)
+		delete panCtl;
+	panCtl = NULL;
+}
+
+int ControlSystem::InitializeServos(int iTiltGPIOidx, int iPanGPIOidx)
+{
+	if (iTiltGPIOidx && iPanGPIOidx)
+	{
+		tiltCtl = new sg90ctl(iTiltGPIOidx);
+		panCtl = new sg90ctl(iPanGPIOidx);
+		if (tiltCtl && panCtl)
+		{
+			int initSts = tiltCtl->initialise();
+			if (initSts) return initSts;
+			initSts = panCtl->initialise();
+			return (-1 * initSts); // invert the number to signal it is an error with Pan servo
+		}
+	}
+	return 1;
+}
+
+int ControlSystem::InitializeCamera(std::string iCascadePath, std::string iNestedCascadePath, double iScale = 1.0)
+{
+	if (iCascadePath.length() > 0)
+	{
+		rpiCam = new FaceTrackingCamera(iCascadePath, iNestedCascadePath, iScale);
+		if (rpiCam)
+			return rpiCam->Initialize();
+	}
+	return 1;
+}
+
+#ifndef __cplusplus
+}
+#endif //ifndef __cplusplus
